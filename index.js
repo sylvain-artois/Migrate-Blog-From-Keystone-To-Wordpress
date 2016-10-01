630c2eec5ebbf6c535750fb2477072368d2eea06
Sylvain Artois <sylvain.artois@gmail.com>
2016-10-01 09:14:58 +0200

    [master] Add dotenv config file

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+
+require('dotenv').config();
+
 const vorpal = require('vorpal')();
 const importer = require('./src/modules/importer');
 
 vorpal
     .command('import', 'Launch the import script')
     .action(function(args, next) {
-        importer.import();
+        importer.run(
+            process.env.WP_JSON_ENDPOINT,
+            process.env.WP_USER,
+            process.env.WP_PASSWORD
+        );
         next();
     });
 
