abdd3335532e7da80e71bb4d95d784af3d313c1a
Sylvain Artois <sylvain.artois@gmail.com>
2016-10-01 09:15:27 +0200

    [master] Add auth and simple post retrieve

--- src/modules/importer.js
+++ src/modules/importer.js
@@ -1,8 +1,67 @@
 
+const WPAPI = require( 'wpapi' );
+
 module.exports = new Importer();
 
-function Importer() {}
+/**
+ * @constructor
+ */
+function Importer() {
+    this.wp = null;
+}
 
-Importer.prototype.import = function () {
-    console.log('import!');
+/**
+ * @param url
+ * @param username
+ * @param password
+ */
+Importer.prototype.run = function (url, username, password) {
+    console.log('import start');
+    authenticate(url, username, password);
+    test();
 };
+
+/**
+ * @param url
+ * @param username
+ * @param password
+ */
+function authenticate(url, username, password) {
+    this.wp = new WPAPI({
+        endpoint: url,
+        username: username,
+        password: password,
+        auth: true,
+        transport: {
+            // Only override the transport for the GET method, in this example
+            // Transport methods should take a wpreq object and a callback:
+            get: function( wpreq, cb ) {
+                let result = cache[ wpreq ];
+                // If a cache hit is found, return it via the same callback/promise
+                // signature as the default transport method:
+                if ( result ) {
+                    if ( cb && typeof cb === 'function' ) {
+                        // Invoke the callback function, if one was provided
+                        cb( null, result );
+                    }
+                    // Return the data as a promise
+                    return Promise.resolve( result );
+                }
+
+                // Delegate to default transport if no cached data was found
+                return WPAPI.transport.get( wpreq, cb ).then(function( result ) {
+                    cache[ wpreq ] = result;
+                    return result;
+                });
+            }
+        }
+    });
+}
+
+function test() {
+    this.wp.posts().then(function( data ) {
+        console.log(data);
+    }).catch(function( err ) {
+        console.error(err);
+    });
+}
