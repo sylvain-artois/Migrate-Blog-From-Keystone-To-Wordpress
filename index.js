0907a3cd76575b379a58ad39dae9e57323899350
Sylvain Artois <sylvain.artois@gmail.com>
2016-10-02 10:23:31 +0200

    [master] Add start and nb argument to cli

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 require('dotenv').config();
 
-const vorpal = require('vorpal')();
-const importer = require('./src/modules/importer');
+const vorpal    = require('vorpal')();
+const importer  = require('./src/modules/importer');
 
 vorpal
     .command('import', 'Launch the import script')
     .action(function(args, next) {
-        importer.run(
+        importer.auth(
             process.env.WP_JSON_ENDPOINT,
             process.env.WP_USER,
             process.env.WP_PASSWORD
         );
+        importer.run( args.start || 0, args.nb || 1);
         next();
     });
 
